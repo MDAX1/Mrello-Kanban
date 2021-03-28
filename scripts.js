a6b573c371ca97b967e3cc1bd1272dc6e7ec635f
Mehrdad <amini_pk@hotmail.com>
2021-03-28 16:10:44 +0200

    stylings changed and modified

--- scripts.js
+++ scripts.js
@@ -1,20 +1,4 @@
-// Sortable (old)
-// ----------------- old code starts here ---------------
-//  $(function() {
-//   $( ".sortable" ).sortable({
-//     connectWith: ".connectedSortable",
-//     receive: function( event, ui ) {
-//       $(this).css({"background-color":"blue"});
-//     }
-//   }).disableSelection(); 
-//   $('.add-button').click(function() {
-//       var txtNewItem = $('#new_text').val();
-//       $(this).closest('div.container').find('ul').append('<li class="card">'+txtNewItem+'</li>');
-//   });    
-// });
-// ----------------- old code ends here ---------------
-
-// Sortable (new code)
+// Sortable
 $(function () {
   $(".sortable1, .sortable2, .sortable3, .sortable4, .sortable5").sortable({
       connectWith: ".connectedSortable"}).disableSelection();
