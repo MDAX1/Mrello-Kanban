f805e4be855afcba868ced12502e456b759df9a8
Mehrdad <amini_pk@hotmail.com>
2021-03-28 16:16:56 +0200

    datepicker & dialog added

--- scripts.js
+++ scripts.js
@@ -1,35 +1,43 @@
-// Sortable
+// sortable
 $(function () {
-  $(".sortable1, .sortable2, .sortable3, .sortable4, .sortable5").sortable({
-      connectWith: ".connectedSortable"}).disableSelection();
+    $(".sortable1, .sortable2, .sortable3, .sortable4, .sortable5").sortable({
+        connectWith: ".connectedSortable"}).disableSelection();
 
-      // datepicker here
+    $("#datepicker").datepicker();
 
-  // Submit task
-  $("#submitBtn").click(function () {
-      // $( "#tabs" ).tabs();
-      var taskList = $("#text").val();
-      // console.log("click", taskList);
-      var dsc = $("#description").val();
-      //append input to the list
-      $(".sortable1").append(
-        "<li class='card'>" + "Title:" + taskList + " " + "<br>" +
-        "Description:" + dsc + "<span> </span>" + "</li>");
-        // dialog here
-    });
-
-  // Input value 
-  $(document).ready(function () {
-      $("#submitBtn").click(function () {
-          var x = $(".link-div").serializeArray();
-          $.each(x, function (i, field) {
-              $("#output").append(field.name + ":" + field.value + " ");
-          });
-
-          // tabs
-          // widget
+    // Submit task
+    $("#submitBtn").click(function () {
+        // $( "#tabs" ).tabs();
+        var taskList = $("#text").val();
+        // console.log("click", taskList);
+        var dsc = $("#description").val();
+        var date = $("#datepicker").val();
+        //append input to the list
+        $(".sortable1").append(
+          "<li class='card'>" + "Title:" + taskList + " " + "<br>" +
+          "Description:" + dsc + "<span> </span> " + "<br>" +
+          "Date:" + date + "</li>");
+        $(".card").click(function () {
+          var infoText = $(this).text();
+          $('#dialog').html(infoText);
+          $('#dialog').dialog();
+        });
+        
       });
 
-  });
+    $(document).ready(function () {
+        $("#submitBtn").click(function () {
+            var x = $(".link-div").serializeArray();
+            $.each(x, function (i, field) {
+                $("#output").append(field.name + ":" + field.value + " ");
+            });
+
+            // dialog
+            // tabs
+            // widget
+            // Minst två valfria effekter från JQuery UI 
+        });
+
+    });
 
 });
