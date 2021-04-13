f325900958b2eead65d87c8eb9f427db297dfd1b
Mehrdad <amini_pk@hotmail.com>
2021-04-13 12:04:06 +0200

    bugfixes

--- scripts.js
+++ scripts.js
@@ -1,43 +1,46 @@
 // sortable
 $(function () {
-    $(".sortable1, .sortable2, .sortable3, .sortable4, .sortable5").sortable({
-        connectWith: ".connectedSortable"}).disableSelection();
+  $(".sortable1, .sortable2, .sortable3, .sortable4, .sortable5").sortable({
+    connectWith: ".connectedSortable"
+  }).disableSelection();
 
-    $("#datepicker").datepicker();
+  $("#datepicker").datepicker();
 
-    // Submit task
+  // Submit task
+  $("#submitBtn").click(function () {
+    // $( "#tabs" ).tabs();
+    var taskList = $("#text").val();
+    // console.log("click", taskList);
+    var dsc = $("#description").val();
+    var date = $("#datepicker").val();
+    $("#tabs").tabs();
+    //append input to the list
+    $(".sortable1").append(
+      "<li class='card'>" + "Title:" + taskList + " " + "<br>" +
+      "Description:" + dsc + "<span> </span> " + "<br>" +
+      "Date:" + date + "</li>");
+
+    $(".card").click(function () {
+      var infoText = $(this).text();
+      $('#dialog').html(infoText);
+      $('#dialog').dialog();
+    });
+
+  });
+
+  $(document).ready(function () {
     $("#submitBtn").click(function () {
-        // $( "#tabs" ).tabs();
-        var taskList = $("#text").val();
-        // console.log("click", taskList);
-        var dsc = $("#description").val();
-        var date = $("#datepicker").val();
-        //append input to the list
-        $(".sortable1").append(
-          "<li class='card'>" + "Title:" + taskList + " " + "<br>" +
-          "Description:" + dsc + "<span> </span> " + "<br>" +
-          "Date:" + date + "</li>");
-        $(".card").click(function () {
-          var infoText = $(this).text();
-          $('#dialog').html(infoText);
-          $('#dialog').dialog();
-        });
-        
+      var x = $(".link-div").serializeArray();
+      $.each(x, function (i, field) {
+        $("#output").append(field.name + ":" + field.value + " ");
       });
 
-    $(document).ready(function () {
-        $("#submitBtn").click(function () {
-            var x = $(".link-div").serializeArray();
-            $.each(x, function (i, field) {
-                $("#output").append(field.name + ":" + field.value + " ");
-            });
-
-            // dialog
-            // tabs
-            // widget
-            // Minst två valfria effekter från JQuery UI 
-        });
-
+      // dialog
+      // tabs
+      // widget
+      // Minst två valfria effekter från JQuery UI 
     });
+
+  });
 
 });
