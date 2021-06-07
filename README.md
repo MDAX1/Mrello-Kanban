37ad0c48b58e10178646f8a45886cad025a6f642
Mehrdad <amini_pk@hotmail.com>
2021-06-07 21:07:57 +0200

    Styling added and bug fixes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Trello-Clone website
 
-This is my attmpt of recreating a trelloclone with jQuery.
-In this task, i build an application that works as a "kanban-board", similar to Trello.com. This is a Frontend project and no backend has been implemented.
+This is my attmpt of recreating a trello clone with jQuery and bootstrap.
+In this task, i build an application that works as a "kanban-board", similar to Trello.com. This is a Frontend project only and no backend has been implemented.
 The idea is that i should apply what i have learned regarding jQuery, jQuery UI in JS and combine that knowledge with previous learning in JS to complete this task.
--- scripts.js
+++ scripts.js
@@ -2,80 +2,81 @@
 $(function () {
 
   $.widget("custom.card", {
-    // Default options.
-    options: {
-      title: ''
-    },
-    // skapar todo items
-    _create: function () {
-      var title = this.options.title;
-      this.element
-        .addClass("card")
-        .addClass("white")
-        .text(title)
+      // Default options.
+      options: {
+          title: ''
+      },
+      // skapar todo items
+      _create: function () {
+          var title = this.options.title;
+          this.element
+              .addClass("card")
+              .addClass("white")
+              .text(title)
 
-      // skapar button open knappen för dialogen
-      this.open = $("<button>", {
-          text: "open info",
-          "class": "open-button"
-        })
+          // skapar button open knappen för dialogen
+          this.open = $("<button>", {
+                  text: "open info",
+                  "class": "open-button"
+              })
 
-        .appendTo(this.element)
-        .button();
-      this._on(this.open, {
-        // _on won't call random when widget is disabled
-        click: "openDialog"
-      });
+              .appendTo(this.element)
+              .button();
+          this._on(this.open, {
+              // _on won't call random when widget is disabled
+              click: "openDialog"
+          });
 
-      // button change card color
-      this.changer = $("<button>", {
-          text: "change",
-          "class": "done"
-        })
+          // button change card color
+          this.changer = $("<button>", {
+                  text: "change",
+                  "class": "done"
+              })
 
-        .appendTo(this.element)
-        .button();
-      this._on(this.changer, {
-        // _on won't call random when widget is disabled
-        click: "done"
-      });
-    },
-    done: function () {
-      this.element.removeClass('white');
-      this.element.addClass('green');
-    },
+              .appendTo(this.element)
+              .button();
+          this._on(this.changer, {
+              // _on won't call random when widget is disabled
+              click: "done"
+          });
+      },
+      done: function () {
+          this.element.removeClass('white');
+          this.element.addClass('green');
+      },
 
-    openDialog: function () {
-      $('#tabs-1').html('<p>' + this.options.description + '</p>')
-      $('#tabs-2').html('<p>' + this.options.date + '</p>')
-      $("#tabs").show();
-      $('#dialog').dialog();
-      $("#dialog").effect("bounce", "slow");
-    }
+      openDialog: function () {
+          $('#tabs-1').html('<p>' + this.options.description + '</p>')
+          $('#tabs-2').html('<p>' + this.options.date + '</p>')
+          $('#tabs-3').html('<p>' + this.options.title + '</p>')
+          $("#tabs").show();
+          $('#dialog').dialog();
+          $("#dialog").effect("bounce", "slow");
+      }
   });
 
-  $(".sortable1, .sortable2, .sortable3, .sortable4, .sortable5").sortable({
-    connectWith: "ul",
-    dropOnEmpty: true
+  $(".sortable1, .sortable2, .sortable3").sortable({
+      connectWith: "ul",
+      dropOnEmpty: true
   }).disableSelection();
 
   $("#datepicker").datepicker();
 
   $("#submitBtn").click(function () {
-    var taskList = $("#text").val();
-    var dsc = $("#description").val();
-    var date = $("#datepicker").val();
+      var taskList = $("#text").val();
+      var dsc = $("#description").val();
+      var date = $("#datepicker").val();
 
-    //append input to the list
-    var liElement = $("<li></li>");
-    liElement.card({
-      title: taskList,
-      description: dsc,
-      date: date
-    });
+      //append input to the list
+      var liElement = $("<li></li>");
+      liElement.card({
+          title: taskList,
+          description: dsc,
+          date: date
+      });
 
-    liElement.toggle("fade", 1000);
-    $(".sortable1").append(liElement);
+      liElement.toggle("fade", 1000);
+      $(".sortable1").append(liElement);
   });
 
   $("#tabs").tabs();
